1dd0d35ca1e69b491f2435d20dbdaee34dc872f9
Kareem Atef <kareematef0102@gmail.com>
2024-05-05 23:23:10 +0300

    Add project files.

---
+++ SQLQueries.sql
@@ -0,0 +1,70 @@
+
+-- 1
+
+-- CREATE DATABASE test;
+
+/* 
+use ITI
+SELECT * FROM Department;
+*/
+
+
+/*
+use test
+CREATE TABLE department (
+	id int,
+	name nvarchar(50),
+	dep_desc nvarchar(100),
+	location nvarchar(50),
+	manager int,
+	m_hire_date date
+)
+insert into department(id, name, location) values (1, 'it', 'mansour'), (2, 'hr', 'mansoura')
+
+select * from department
+
+drop table department
+*/
+
+-------------------
+
+
+-- 3
+
+/*
+use test
+CREATE TABLE student(
+	St_Id int,
+	St_Fname nvarchar(50),
+	St_Lname nchar(10),
+	St_Address nvarchar(100),
+	St_Age int,
+	Dept_Id int,
+	St_super int
+)
+
+
+select * from std;
+
+*/
+
+
+---------
+
+
+-- 4
+
+/*
+use ITI
+select * from Course
+
+insert into Course(Crs_Id, Crs_Duration) values (1, 2), (2, 3), (3, 3), (4, 3), (5, 1), (6, 0);
+
+*/
+
+
+
+---------------------
+
+
+
